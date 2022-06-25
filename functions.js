f0aeeeb5366c6fcf713175ba387f7415fbabfcea
vijayakumargopal <75659118+vijayakumargopal@users.noreply.github.com>
2022-06-25 16:26:56 +0530

    Update functions.js

--- functions.js
+++ functions.js
@@ -2,4 +2,7 @@ function active_status_change(e, data){
 	var actived = document.getElementsByClassName("active");
 	actived[0].className = "";
 	data.parentElement.className = "active";
+	document.getElementById("myNavbar").setAttribute("aria-expanded","false");
+	document.getElementById("myNavbar").className= "navbar-collapse collapse";
+
 }
